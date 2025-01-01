b788aa562a0edee64c49f995b546ee8c91caf888
tomsawyer0224 <binha0224@gmail.com>
2025-01-01 15:48:20 +0700

    modify

--- trainer.py
+++ trainer.py
@@ -1,7 +1,8 @@
 import mlflow
 from sklearn.linear_model import SGDClassifier
 from datetime import datetime
-from mlflow.models import infer_signature
+from mlflow.models import infer_signature, validate_serving_input
+import pandas as pd
 from data_module import Data_Module
 from pyfunc_flavor import MLflowModel
 
@@ -9,9 +10,16 @@ class Trainer:
     def __init__(self, use_pyfunc_flavor = False):
         mlflow.set_tracking_uri(uri="http://127.0.0.1:8000")
         self.experiment_id = self._get_or_create_experiment("Test_MLflow_Deployment")
+        self.run_name = f"model_at_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+        self.run_id = None
         self.datasets = Data_Module()
         self.model = SGDClassifier()
         self.use_pyfunc_flavor = use_pyfunc_flavor
+    @property
+    def model_uri(self):
+        model_name = "pyfunc_model" if self.use_pyfunc_flavor else "sklearn_model"
+        model_uri = f"runs:/{self.run_id}/{model_name}"
+        return model_uri
     def _get_or_create_experiment(self, experiment_name: str) -> str:
         """
         gets an existing experiment or creates a new experiment
@@ -29,8 +37,8 @@ class Trainer:
         train_data, train_target = self.datasets.train_dataset
         input_example = train_data[:2]
         signature = infer_signature(train_data[:2], train_target[:2])
-        run_name = f"model_at_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-        with mlflow.start_run(experiment_id=self.experiment_id, run_name=run_name):
+        #run_name = f"model_at_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+        with mlflow.start_run(experiment_id=self.experiment_id, run_name=self.run_name) as run:
             self.model.fit(train_data, train_target)
             if not self.use_pyfunc_flavor:
                 mlflow.sklearn.log_model(
@@ -46,11 +54,48 @@ class Trainer:
                 signature = signature,
                 input_example = input_example
                 )
+        self.run_id = run.info.run_id
     def test(self):
-        pass
+        serving_payload = """{
+"dataframe_split": {
+"columns": [
+    "sepal length (cm)",
+    "sepal width (cm)",
+    "petal length (cm)",
+    "petal width (cm)"
+],
+"data": [
+    [
+    4.6,
+    3.6,
+    1.0,
+    0.2
+    ],
+    [
+    5.7,
+    4.4,
+    1.5,
+    0.4
+    ]
+]
+}
+}"""
+        # Validate the serving payload works on the model
+        print("validate")
+        predictions = validate_serving_input(self.model_uri, serving_payload)
+        print(predictions)
+        print("-"*30)
+        print("inference")
+        test_data, test_target = self.datasets.test_dataset
+        loaded_model = mlflow.pyfunc.load_model(self.model_uri)
+        preds = loaded_model.predict(test_data)
+        print(preds)
+        print(f"score = {self.model.score(test_data, test_target)}")
 if __name__=="__main__":
     trainer_sklearn = Trainer(use_pyfunc_flavor=False)
     trainer_sklearn.train()
+    trainer_sklearn.test()
 
     trainer_pyfunc = Trainer(use_pyfunc_flavor=True)
     trainer_pyfunc.train()
+    trainer_pyfunc.test()
